d056eabf3f830d2395bf3e14c3461c255f0a0632
ss22124 <128476655+ss22124@users.noreply.github.com>
2023-08-12 23:52:39 +0100

    Add files via upload

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,16 +4,16 @@ import pandas as pd
 import pandas as pd
 import os
 # 获取当前文件所在的目录路径
-notebook_path = os.path.abspath('data_preprocessing.py')
-current_directory = os.path.dirname(notebook_path)
+# notebook_path = os.path.abspath('data_preprocessing.py')
+# current_directory = os.path.dirname(notebook_path)
 
-# 构建数据文件的路径
-file1_path = os.path.join(current_directory, "../data/accepted_2007_to_2018q4.csv", "accepted_2007_to_2018q4.csv")
+# # 构建数据文件的路径
+# file1_path = os.path.join(current_directory, "../data/accepted_2007_to_2018q4.csv", "accepted_2007_to_2018q4.csv")
 
 print("1")
 # 读取file1.csv
 #file1_path = 'D:/bristol/study/subjects/individual project/data_lendingclub/2/archive/accepted_2007_to_2018q4.csv/accepted_2007_to_2018q4.csv'
-data1 = pd.read_csv(file1_path)
+data1 = pd.read_csv("../data/accepted_2007_to_2018q4.csv/accepted_2007_to_2018q4.csv")
 print("2")
 # 添加新列，并填充为1
 data1['New_Column_statement'] = 1
@@ -23,11 +23,11 @@ data1.to_csv('processing_data1.csv', index=False)
 
 print("4")
 # 构建数据文件的路径
-file2_path = os.path.join(current_directory, "../data/rejected_2007_to_2018q4.csv", "rejected_2007_to_2018q4.csv")
+# file2_path = os.path.join(current_directory, "../data/rejected_2007_to_2018q4.csv", "rejected_2007_to_2018q4.csv")
 
 # 读取file2.csv
 #file2_path = 'D:/bristol/study/subjects/individual project/data_lendingclub/2/archive/rejected_2007_to_2018q4.csv/rejected_2007_to_2018q4.csv'
-data2 = pd.read_csv(file2_path)
+data2 = pd.read_csv("../data/rejected_2007_to_2018q4.csv/rejected_2007_to_2018q4.csv")
 print("5")
 # 添加新列，并填充为0
 data2['New_Column_statement'] = 0
